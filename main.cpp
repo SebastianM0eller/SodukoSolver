619e3875ac5f381175e410a15427cc5ccbab7dd1
Sebastian Moeller <SebastianMOlesen@gmail.com>
2026-01-06 15:12:52 +0100

    Made it so the user is asked for the soduko they want to solve, in the terminal

--- main.cpp
+++ main.cpp
@@ -9,7 +9,12 @@
 
 int main()
 {
-  Grid SodukoBoard{};
+  std::string boardInput;
+
+  std::cout << "Enter a Sudoku board (9x9 numbers) as a continuous string: ";
+  std::cin >> boardInput;
+
+  Grid SodukoBoard{boardInput};
   SolverStruct solvedSoduko = Solve(SodukoBoard, 0);
   solvedSoduko.PrintBoard();
 }
